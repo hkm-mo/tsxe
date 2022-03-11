4c69cc2324156ff9faff0f36019aced6414b0a76
hkm-mo <hkmcss@gmail.com>
2022-03-12 02:15:58 +0800

    Export named TSXe object for backwards compatible

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,20 @@
  * @see {@link https://github.com/hkm-mo/tsxe|GitHub}
  * @license MIT
  */
-import * as TSXe from "./TSXe";
+import * as TSXeObj from "./TSXe"
 export * from "./TSXe";
-export default TSXe;
+export default TSXeObj;
+
+/**
+ * @deprecated Please use `import TSXe from "tsxe"` instead
+ */
+export const TSXe = TSXeObj;
+
+/**
+ * @deprecated Please use `import TSXe from "tsxe"` instead
+ */
+export namespace TSXe {
+    export type TSXeNode = TSXeObj.TSXeNode;
+    export type Fragment = TSXeObj.Fragment;
+    export type Component = TSXeObj.Component;
+}
