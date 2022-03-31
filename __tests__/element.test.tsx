204aa78f0ea6056ff3f4c68570b4423efd2e7c82
hkm-mo <hkmcss@gmail.com>
2022-04-01 00:36:23 +0800

    Add elements creation test cases

---
+++ __tests__/element.test.tsx
@@ -0,0 +1,62 @@
+import TSXe from "../dist"
+
+test("create element", () => {
+    const helloWorld = "Hello World";
+    const elmId = "hello";
+
+    document.body.appendChild(<div id={elmId}>{helloWorld}</div>);
+
+    const elm = document.getElementById(elmId);
+    
+    expect(elm).not.toBeNull();
+    expect(elm?.textContent).toBe(helloWorld);
+});
+
+test("nested array", () => {
+    const children = [
+        ["Hello "],
+        [["World", [<span>!</span>]]]
+    ];
+
+    document.body.appendChild(<div id="nestedArray">{children}</div>);
+
+    const elm = document.getElementById("nestedArray");
+
+    expect(elm?.innerHTML).toBe("Hello World<span>!</span>");
+});
+
+
+test("create Ref object", () => {
+    const elmId = "refObjDiv";
+    const ref = TSXe.createRef<HTMLDivElement>();
+
+    document.body.appendChild(<div id={elmId} ref={ref}></div>);
+
+    const elm = document.getElementById(elmId);
+
+    expect(elm).toBe(ref.current);
+});
+
+
+test("create Ref func", () => {
+    const elmId = "refFuncDiv";
+    const ref = (ref: HTMLDivElement) => {
+        const elm = document.getElementById(elmId);
+        expect(elm).toBe(ref);
+    };
+
+    document.body.appendChild(<div id={elmId} ref={ref}></div>);
+});
+
+test("fragment", () => {
+    document.body.innerHTML = "";
+    document.body.appendChild(
+        <>
+            <div></div>
+            <div></div>
+            <div><span></span></div>
+        </>
+    );
+
+    expect(document.body.childNodes.length).toBe(3);
+});
